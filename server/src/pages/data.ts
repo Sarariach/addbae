c4c1aa718ee3998eda5b7c75e9f5a8d6b2b1c303
SaraRiach <saracar2@gmail.com>
2018-04-18 10:31:55 +0200

    dummy data

---
+++ server/src/pages/data.ts
@@ -0,0 +1,30 @@
+const pagesById = {
+    1: {
+        id: 1,
+        title: 'Buy Harry',
+        description: 'Harry Potter books',
+        picture:'https://prodimage.images-bn.com/pimages/9780545162074_p0_v2_s550x406.jpg',
+        price: 20,
+        email: 'callme@harry.com',
+        phone: 6254189000
+    },
+    2: {
+        id: 2,
+        title: 'These Boots are mad for walking',
+        description: 'Nice Vegan Boots',
+        picture:'https://www.scorpioshoes.com/images/dr-martens-castel-womens-8-eyelet-vegan-boots-gunmetal-grey-p46108-67445_image.jpg',
+        price: 100,
+        email: 'buyit@boots.com',
+        phone: 696669666
+    },
+    3: {
+        title: 'Live the Luxurious Life',
+        description: 'Buy all the expensive brands for cheap',
+        picture:'https://www.top10twist.com/wp-content/uploads/2017/07/top10expensive-cloth-brands-min-1024x509.png',
+        price: 50,
+        email: 'gucci@gang.com',
+        phone: 8990027375357
+    }
+  }
+  
+  export default pagesById
