073eb21fc2902ce68a03906cc1f12ae7663c57af
SaraRiach <saracar2@gmail.com>
2018-04-18 11:30:21 +0200

    trying to connect with db

---
+++ server/src/Adds/controller.ts
@@ -0,0 +1,36 @@
+import {JsonController, Get, Param, Put, Post, Body, HttpCode, NotFoundError} from 'routing-controllers'
+import Add from './entity'
+
+@JsonController()
+export default class AddController {
+
+@Get('/adds/:id')
+getAdd(
+    @Param('id') id: number
+) {
+  return Add.findOneById(id)
+}
+@Get('/adds')
+async allAdds() {
+  const adds = await Add.find()
+  return { adds }
+}
+@Put('/adds/:id')
+async updateAdd(
+    @Param('id') id: number,
+    @Body() update: Partial<Add>
+) {
+    const add = await Add.findOneById(id)
+    if (!add) throw new NotFoundError('Cannot find add')
+        return Add.merge(add, update).save()
+    }
+
+@Post('/adds')
+@HttpCode(201)
+createPage(
+@Body() body: Add
+): Add {
+    console.log(`Incoming POST body param:`, body)
+    return body
+}
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,18 @@
 import 'reflect-metadata' 
 import {createKoaServer} from 'routing-controllers'
-import Controller from './controller'
+import AddController from './adds/controller'
+import setupDb from './db'
 
 const port = process.env.PORT || 4000
 
 const app = createKoaServer({
-    controllers: [Controller]
+    controllers: [
+        AddController
+    ]
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
